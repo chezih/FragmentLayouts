f705797979aa3f3cb65979df538dda1ed89afcb9
chezih <chezih@gmail.com>
2015-05-18 00:32:42 +0300

    Initial commit

---
+++ app/src/main/java/com/chamud/cheziandsima/fragmentlayouts/FragmentLayout.java
@@ -0,0 +1,15 @@
+package com.chamud.cheziandsima.fragmentlayouts;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FragmentLayout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the layout for fragment_layout.xml
+        setContentView(R.layout.fragment_layout);
+    }
+
+}
